1ebeda4df4d2c03f70db6fb5bd35bc610350ef04
Keiji-morita <keijim3116@gmail.com>
2022-04-01 18:16:12 +0900

    開発準備

---
+++ lib/planList.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class planList extends StatefulWidget {
+  @override
+  _planListState createState() => _planListState();
+}
+
+class _planListState extends State<planList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body: PageView(),
+    );
+  }
+}
