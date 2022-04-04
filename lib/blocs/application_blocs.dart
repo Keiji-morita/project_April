59978e6f9f06b18897bb6529c88b5af29e90a9aa
Keiji-morita <keijim3116@gmail.com>
2022-04-04 16:13:58 +0900

    地図の実装成功

---
+++ lib/blocs/application_blocs.dart
@@ -0,0 +1,12 @@
+import 'package:aprilprojectapp/seveces/googlelocator_sevece.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Applicationbloc with ChangeNotifier {
+  final geolocatorservice = GeolocatorSeries();
+
+  //variable
+  // Position currentLocation;
+
+  setCurrentLocation() async {}
+}
--- lib/serch.dart
+++ lib/serch.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:geolocator/geolocator.dart';
 
 class serch extends StatefulWidget {
   @override
@@ -12,7 +15,25 @@ class _serchState extends State<serch> {
       appBar: AppBar(
         title: Text("serch start and goal"),
       ),
-      body: PageView(),
+      body: ListView(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: "serch location!",
+            ),
+          ),
+          Container(
+            height: 300.0,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              myLocationEnabled: true,
+              initialCameraPosition: 
+                CameraPosition(target: LatLng(41.8781, -87.6298)),
+            ),
+          )
+          
+        ],
+      ),
     );
   }
 }
